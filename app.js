fc315f368c561b1614d2f048217b4bad638ad9a6
ayeshaqadri <qadria392@gmail.com>
2024-10-28 20:58:59 +0500

    incomplete

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let type = new Typed(".txt-1", {
-    strings: ["Web and app developer", "Code perfectionist", "Strategic thinker"],
+    strings: ["Web and app developer...", "Code perfectionist...", "Strategic thinker..."],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,  // Added some delay between typing cycles
