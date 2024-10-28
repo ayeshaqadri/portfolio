74485ff5673dab3c7a01b8366053bb49d7825609
ayeshaqadri <qadria392@gmail.com>
2024-10-28 15:27:07 +0500

    incomplete

---
+++ app.js
@@ -0,0 +1,7 @@
+let type = new Typed(".txt-1", {
+    strings: ["Web and app developer", "Code perfectionist", "Strategic thinker"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,  // Added some delay between typing cycles
+    loop: true
+});
